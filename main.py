a08d6cec8d84e56258018c8c332dba6323b6bb08
Rafael Kurniawan <rafael.kurniawan@binus.ac.id>
2024-06-16 14:16:36 +0700

    change server run address

--- main.py
+++ main.py
@@ -228,6 +228,9 @@ def feedback():
 
     return jsonify(success=True)
 
+import os
+
 if __name__ == "__main__":
     print("Starting the server and loading the model...")
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
